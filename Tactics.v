206e9befdde8b019d31dab7ae53785851d6eaee6
uni82 <mayu1368@gmail.com>
2021-10-04 13:38:48 +0900

    Solve plus_n_n_injective

--- Tactics.v
+++ Tactics.v
@@ -1,2 +1,192 @@
 Set Warnings "-notation-overridden,-parsing".
 From LF Require Export Poly.
+Require Import PeanoNat.
+
+Theorem silly1 : forall (n m o p : nat),
+     n = m ->
+     [n;o] = [n;p] ->
+     [n;o] = [m;p].
+Proof.
+  intros n m o p eq1 eq2.
+  rewrite <- eq1.
+  apply eq2. Qed.
+
+Theorem silly2 : forall (n m o p : nat),
+     n = m ->
+     (forall (q r : nat), q = r -> [q;o] = [r;p]) ->
+     [n;o] = [m;p].
+Proof.
+  intros n m o p eq1 eq2.
+  apply eq2. apply eq1. Qed.
+
+Theorem silly2a : forall (n m : nat),
+     (n,n) = (m,m) ->
+     (forall (q r : nat), (q,q) = (r,r) -> [q] = [r]) ->
+     [n] = [m].
+Proof.
+  intros n m eq1 eq2.
+  apply eq2. apply eq1. Qed.
+
+ (* standard, optional (silly_ex) *)
+Theorem silly_ex :
+     (forall n, evenb n = true -> oddb (S n) = true) ->
+     oddb 3 = true ->
+     evenb 4 = true.
+Proof.
+  intros eq1 eq2. apply eq2. Qed. 
+ (*  /standard, optional (silly_ex) *)
+
+ (* standard (apply_exercise1) *)
+Theorem rev_exercise1 : forall (l l' : list nat),
+     l = rev l' ->
+     l' = rev l.
+Proof.
+  intros l l' eq. symmetry. rewrite -> eq. Search rev. apply rev_involutive. Qed. 
+ (* /standard (apply_exercise1) *)
+
+Example trans_eq_example : forall (a b c d e f : nat),
+     [a;b] = [c;d] ->
+     [c;d] = [e;f] ->
+     [a;b] = [e;f].
+Proof.
+  intros a b c d e f eq1 eq2.
+  rewrite -> eq1. rewrite -> eq2. reflexivity. Qed.
+
+Theorem trans_eq : forall (X:Type) (n m o : X),
+  n = m -> m = o -> n = o.
+Proof.
+  intros X n m o eq1 eq2. rewrite -> eq1. rewrite -> eq2.
+  reflexivity. Qed.
+ 
+Example trans_eq_example' : forall (a b c d e f : nat),
+     [a;b] = [c;d] ->
+     [c;d] = [e;f] ->
+     [a;b] = [e;f].
+Proof.
+  intros a b c d e f eq1 eq2.
+  apply trans_eq with (m:=[c;d]).
+  apply eq1. apply eq2. Qed.
+
+
+ (* standard, optional (apply_with_exercise) *)
+Example trans_eq_exercise : forall (n m o p : nat),
+     m = (minustwo o) ->
+     (n + p) = m ->
+     (n + p) = (minustwo o).
+Proof. 
+  intros n m o p eq1 eq2. apply trans_eq with m.
+  apply eq2. apply eq1. Qed.
+ (*  /standard, optional (apply_with_exercise) *)
+
+Theorem S_injective : forall (n m : nat),
+  S n = S m ->
+  n = m.
+Proof.
+  intros n m H1.
+  assert (H2: n = pred (S n)). { reflexivity. }
+  rewrite H2. rewrite H1. reflexivity.
+Qed.
+
+Theorem S_injective' : forall (n m : nat),
+  S n = S m ->
+  n = m.
+Proof.
+  intros n m H.
+  injection H. intros Hnm. apply Hnm. Qed.
+
+Theorem injection_ex1 : forall (n m o : nat),
+  [n; m] = [o; o] ->
+  [n] = [m].
+Proof.
+  intros n m o H.
+  injection H. intros H1 H2.
+  rewrite H1. rewrite H2. reflexivity.
+Qed.
+
+Theorem injection_ex2 : forall (n m : nat),
+  [n] = [m] ->
+  n = m.
+Proof.
+  intros n m H.
+  injection H as Hnm. rewrite Hnm.
+  reflexivity. Qed.
+
+
+ (*  standard (injection_ex3) *)
+Example injection_ex3 : forall (X : Type) (x y z : X) (l j : list X),
+  x :: y :: l = z :: j ->
+  y :: l = x :: j ->
+  x = y.
+Proof.
+  intros X x y z l j eq1 eq2.
+  injection eq2. intros h1 h2. symmetry. apply h2. Qed.
+ (*  /standard (injection_ex3) *)
+
+Theorem eqb_0_l : forall n,
+   0 =? n = true -> n = 0.
+Proof.
+  intros n.
+  destruct n as [| n'] eqn:E.
+  - intros H. reflexivity.
+  - simpl. intros H. discriminate H.
+Qed.
+
+Theorem discriminate_ex1 : forall (n : nat),
+  S n = O ->
+  2 + 2 = 5.
+Proof.
+  intros n contra. discriminate contra. Qed.
+
+Theorem discriminate_ex2 : forall (n m : nat),
+  false = true ->
+  [n] = [m].
+Proof.
+  intros n m contra. discriminate contra. Qed.
+
+(*standard (discriminate_ex3)*)
+Example discriminate_ex3 :
+  forall (X : Type) (x y z : X) (l j : list X),
+    x :: y :: l = [] ->
+    x = z.
+Proof.
+  intros X x y z l j H. discriminate H. Qed.
+(* /standard (discriminate_ex3)*)
+
+Theorem f_equal : forall (A B : Type) (f: A -> B) (x y: A),
+  x = y -> f x = f y.
+Proof. intros A B f x y eq. rewrite eq. reflexivity. Qed.
+
+Theorem S_inj : forall (n m : nat) (b : bool),
+     (S n) =? (S m) = b ->
+     n =? m = b.
+Proof.
+  intros n m b H. simpl in H. apply H. Qed.
+
+Theorem silly3' : forall (n : nat),
+  (n =? 5 = true -> (S (S n)) =? 7 = true) ->
+  true = (n =? 5) ->
+  true = ((S (S n)) =? 7).
+Proof.
+  intros n eq H.
+  symmetry in H. apply eq in H. symmetry in H.
+  apply H. Qed.
+
+
+(*standard, recommended (plus_n_n_injective)*)
+Theorem plus_n_n_injective : forall n m,
+     n + n = m + m ->
+     n = m.
+Proof.
+  intros n. induction n as [| n'].
+  - intros m H. induction m as [| m'].
+    + reflexivity.
+    + discriminate H.
+  - intros m H. induction m as [| m'].
+    + discriminate H.
+    + simpl in H. rewrite <- plus_n_Sm in H. rewrite <- plus_n_Sm in H. 
+      inversion H. apply IHn' in H1. rewrite -> H1. reflexivity. Qed.  
+(*plus_n_Sm: forall n m : nat, S (n + m) = n + S m*)
+(*/standard, recommended (plus_n_n_injective)*)
+
+
+
